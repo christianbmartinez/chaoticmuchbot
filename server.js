055d23663453c05065898460990a0a1c8fb99900
Christian Martinez <christian@mixdlabs.com>
2022-12-08 20:35:54 -0700

    asd

--- server.js
+++ server.js
@@ -31,61 +31,61 @@ async function getLatestTweet() {
 }
 getLatestTweet()
 
-let apexStats = 'Failed to fetch current apex stats'
-
-async function getApexStats() {
-  try {
-    const response = await axios.get(
-      `https://api.mozambiquehe.re/bridge?auth=${process.env.APEX_STATS_AUTH}&uid=1002896792850&platform=PC`
-    )
-    apexStats = `
-    Rank: ${
-      response.data.global.rank.rankName
-        ? response.data.global.rank.rankName
-        : undefined
-    }, 
-    RP: ${
-      response.data.global.rank.rankScore
-        ? response.data.global.rank.rankScore
-        : undefined
-    }, 
-    Position:# ${
-      response.data.global.rank.ladderPosPlatform
-        ? response.data.global.rank.ladderPosPlatform
-        : undefined
-    }, 
-    Legend: ${
-      response.data.legends.selected.LegendName
-        ? response.data.legends.selected.LegendName
-        : undefined
-    }, 
-    Legend Kills: ${
-      response.data.legends.selected.data[0].value
-        ? response.data.legends.selected.data[0].value
-        : undefined
-    }, 
-    Skin: ${
-      response.data.legends.selected.gameInfo.skin
-        ? response.data.legends.selected.gameInfo.skin
-        : undefined
-    }, 
-    Pose: ${
-      response.data.legends.selected.gameInfo.pose
-        ? response.data.legends.selected.gameInfo.pose
-        : undefined
-    }, 
-    Frame: ${
-      response.data.legends.selected.gameInfo.frame
-        ? response.data.legends.selected.gameInfo.frame
-        : undefined
-    }`
-    console.log('Got apex data')
-  } catch (error) {
-    console.error(error)
-  }
-}
-getApexStats()
-setInterval(getApexStats, 300000)
+//let apexStats = 'Failed to fetch current apex stats'
+//
+//async function getApexStats() {
+//  try {
+//    const response = await axios.get(
+//      `https://api.mozambiquehe.re/bridge?auth=${process.env.APEX_STATS_AUTH}&uid=1002896792850&platform=PC`
+//    )
+//    apexStats = `
+//    Rank: ${
+//      response.data.global.rank.rankName
+//        ? response.data.global.rank.rankName
+//        : undefined
+//    }, 
+//    RP: ${
+//      response.data.global.rank.rankScore
+//        ? response.data.global.rank.rankScore
+//        : undefined
+//    }, 
+//    Position:# ${
+//      response.data.global.rank.ladderPosPlatform
+//        ? response.data.global.rank.ladderPosPlatform
+//        : undefined
+//    }, 
+//    Legend: ${
+//      response.data.legends.selected.LegendName
+//        ? response.data.legends.selected.LegendName
+//        : undefined
+//    }, 
+//    Legend Kills: ${
+//      response.data.legends.selected.data[0].value
+//        ? response.data.legends.selected.data[0].value
+//        : undefined
+//    }, 
+//    Skin: ${
+//      response.data.legends.selected.gameInfo.skin
+//        ? response.data.legends.selected.gameInfo.skin
+//        : undefined
+//    }, 
+//    Pose: ${
+//      response.data.legends.selected.gameInfo.pose
+//        ? response.data.legends.selected.gameInfo.pose
+//        : undefined
+//    }, 
+//    Frame: ${
+//      response.data.legends.selected.gameInfo.frame
+//        ? response.data.legends.selected.gameInfo.frame
+//        : undefined
+//    }`
+//    console.log('Got apex data')
+//  } catch (error) {
+//    console.error(error)
+//  }
+//}
+//getApexStats()
+//setInterval(getApexStats, 300000)
 
 let degrees
 
@@ -232,10 +232,10 @@ client2.on('message', (channel, tags, message, self) => {
     tourneyIsActive = false
   }
 
-  if (message.includes('!livestats')) {
-    client2.say(channel, `@${tags.username}, ${apexStats}`)
-    getApexStats()
-  }
+  //if (message.includes('!livestats')) {
+  //  client2.say(channel, `@${tags.username}, ${apexStats}`)
+  //  getApexStats()
+  //}
 
   if (message.includes('!pickupline')) {
     client2.say(channel, `@${tags.username}, ${pickupLine}`)
