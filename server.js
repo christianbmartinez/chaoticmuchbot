674ca970d0f6cf84bf7a156457465ac98f5cd9cb
Christian Martinez <christian@mixdlabs.com>
2023-03-18 15:47:32 -0600

    afd

--- server.js
+++ server.js
@@ -388,5 +388,5 @@ runCompletion(message)
       `@${isWinner} has already been chosen as our giveaway winner! We appreciate everyone joining the giveaway! :)`
     )
   }
-  console.log(`${tags['display-name']}: ${message}, IS VIP? ${tags.badges.vip ? tags.badges.vip : 'No'}`)
+  console.log(`${tags['display-name']}: ${message}, IS VIP? ${tags.badges.vip !== null || undefined || false || 0 ? true : false}`)
 })
