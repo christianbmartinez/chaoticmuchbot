d4dbb8b2e453daf00532a80b0af2c8462f71de78
Christian Martinez <christian@mixdlabs.com>
2023-08-08 10:58:26 -0600

    Turned on apex stats

--- server.js
+++ server.js
@@ -34,67 +34,74 @@ const openai = new OpenAIApi(configuration)
 //    throw new Error('Unsuccessful request')
 //  }
 //}
-//getLatestTweet()
+//getLatestTweet(
+let apexStats = 'Failed to fetch current apex stats'
 
-//let apexStats = 'Failed to fetch current apex stats'
-//
-//async function getApexStats() {
-//  try {
-//    const response = await axios.get(
-//      `https://api.mozambiquehe.re/bridge?auth=${process.env.APEX_STATS_AUTH}&uid=1006162359940&platform=PC`
-//    )
-//    apexStats = `
-//    Rank: ${
-//      response.data.global.rank.rankName
-//        ? response.data.global.rank.rankName
-//        : undefined
-//    },
-//    RP: ${
-//      response.data.global.rank.rankScore
-//        ? response.data.global.rank.rankScore
-//        : undefined
-//    },
-//    Position:# ${
-//      response.data.global.rank.ladderPosPlatform
-//        ? response.data.global.rank.ladderPosPlatform
-//        : undefined
-//    },
-//    Legend: ${
-//      response.data.legends.selected.LegendName
-//        ? response.data.legends.selected.LegendName
-//        : undefined
-//    },
-//    Legend Kills: ${
-//      response.data.legends.selected.data[0].value
-//        ? response.data.legends.selected.data[0].value
-//        : undefined
-//    },
-//    Skin: ${
-//      response.data.legends.selected.gameInfo.skin
-//        ? response.data.legends.selected.gameInfo.skin
-//        : undefined
-//    },
-//    Pose: ${
-//      response.data.legends.selected.gameInfo.pose
-//        ? response.data.legends.selected.gameInfo.pose
-//        : undefined
-//    },
-//    Frame: ${
-//      response.data.legends.selected.gameInfo.frame
-//        ? response.data.legends.selected.gameInfo.frame
-//        : undefined
-//    }`
-//    console.log('Got apex data:', response.data.global.rank.rankName,response.data.global.rank.rankScore, response.data.global.rank.ladderPosPlatform,
-//    response.data.legends.selected.LegendName, response.data.legends.selected.data[0].value, response.data.legends.selected.gameInfo.skin,
-//    response.data.legends.selected.gameInfo.pose, response.data.legends.selected.gameInfo.frame  )
-//  } catch (error) {
-//    console.error(error)
-//  }
-//}
-//getApexStats()
-//setInterval(() => {
-//  getApexStats()
-//}, 1000 * 60 )
+async function getApexStats() {
+  try {
+    const response = await axios.get(
+      `https://api.mozambiquehe.re/bridge?auth=${process.env.APEX_STATS_AUTH}&uid=1006162359940&platform=PC`
+    )
+    apexStats = `
+    Rank: ${
+      response.data.global.rank.rankName
+        ? response.data.global.rank.rankName
+        : undefined
+    },
+    RP: ${
+      response.data.global.rank.rankScore
+        ? response.data.global.rank.rankScore
+        : undefined
+    },
+    Position:# ${
+      response.data.global.rank.ladderPosPlatform
+        ? response.data.global.rank.ladderPosPlatform
+        : undefined
+    },
+    Legend: ${
+      response.data.legends.selected.LegendName
+        ? response.data.legends.selected.LegendName
+        : undefined
+    },
+    Legend Kills: ${
+      response.data.legends.selected.data[0].value
+        ? response.data.legends.selected.data[0].value
+        : undefined
+    },
+    Skin: ${
+      response.data.legends.selected.gameInfo.skin
+        ? response.data.legends.selected.gameInfo.skin
+        : undefined
+    },
+    Pose: ${
+      response.data.legends.selected.gameInfo.pose
+        ? response.data.legends.selected.gameInfo.pose
+        : undefined
+    },
+    Frame: ${
+      response.data.legends.selected.gameInfo.frame
+        ? response.data.legends.selected.gameInfo.frame
+        : undefined
+    }`
+    console.log(
+      'Got apex data:',
+      response.data.global.rank.rankName,
+      response.data.global.rank.rankScore,
+      response.data.global.rank.ladderPosPlatform,
+      response.data.legends.selected.LegendName,
+      response.data.legends.selected.data[0].value,
+      response.data.legends.selected.gameInfo.skin,
+      response.data.legends.selected.gameInfo.pose,
+      response.data.legends.selected.gameInfo.frame
+    )
+  } catch (error) {
+    console.error(error)
+  }
+}
+getApexStats()
+setInterval(() => {
+  getApexStats()
+}, 1000 * 60)
 
 let degrees
 
@@ -233,10 +240,10 @@ client2.on('message', (channel, tags, message, self) => {
     tourneyIsActive = false
   }
 
-  //if (message.includes('!livestats')) {
-  //  client2.say(channel, `@${tags.username}, ${apexStats}`)
-  //  getApexStats()
-  //}
+  if (message.includes('!livestats')) {
+    client2.say(channel, `@${tags.username}, ${apexStats}`)
+    getApexStats()
+  }
 
   if (
     message.includes('@chaoticmuchbot') &&
