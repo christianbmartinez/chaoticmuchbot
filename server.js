a62354e463a42bba33fc2059f7fde25716436fe9
Christian Martinez <christian@mixdlabs.com>
2023-08-08 11:04:56 -0600

    Updated zipcode to match city where chaotic lives in temecula, ca

--- server.js
+++ server.js
@@ -108,7 +108,7 @@ let degrees
 async function getDegrees() {
   try {
     const response = await axios.get(
-      `http://api.openweathermap.org/data/2.5/weather?zip=90001&appid=${process.env.WEATHER_APP_ID}&units=imperial`
+      `http://api.openweathermap.org/data/2.5/weather?zip=92589&appid=${process.env.WEATHER_APP_ID}&units=imperial`
     )
     console.log('Got degrees data')
     degrees = response.data.main.temp
@@ -123,7 +123,7 @@ let celcius
 async function getCelcius() {
   try {
     const response = await axios.get(
-      `http://api.openweathermap.org/data/2.5/weather?zip=90001&appid=${process.env.WEATHER_APP_ID}&units=metric`
+      `http://api.openweathermap.org/data/2.5/weather?zip=92589&appid=${process.env.WEATHER_APP_ID}&units=metric`
     )
     console.log('Got celcius data')
     celcius = response.data.main.temp
